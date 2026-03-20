120e932da36c57e26cc3d32c777828c297e06c64
Jose Eduardo Madrigal Osorio <75703758+Eduardo-Osoi@users.noreply.github.com>
2026-03-19 20:49:48 -0600

    Actualizar python2.py

--- python2.py
+++ python2.py
@@ -9,19 +9,18 @@ running_total = appetizers + main_courses + desserts + drinks
 
 print("Total bill so far:", running_total)
 
-# Calcular la propina del 25%
+
 tip = running_total * 0.25
 print("Tip amount:", tip)
 
-# Agregar la propina al total usando el operador +=
+
 running_total += tip
 print("Total with tip:", running_total)
 
-# Dividir el total entre el número de amigos
+
 final_bill = running_total / num_of_friends
 
-# Redondear a dos decimales
+
 each_pays = round(final_bill, 2)
 
-# Mostrar cuánto paga cada persona con dos decimales
 print("Each person pays:", each_pays)
